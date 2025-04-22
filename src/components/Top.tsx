3395e1c1c62d8bf133f31e8d650af8397554b7af
鈴木友也 <suzy_tomy@docomo.ne.jp>
2025-04-23 04:20:33 +0900

    画像一覧

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,13 +1,14 @@
+import { useRef } from "react";
 import CharacterDisplay from "./characters/CharacterDisplay";
 import TaskList from "./todo/TaskList";
 
 const Top = () => {
-
+  const characterDisplayRef = useRef<{ refreshCharacterData: () => void }>(null);
 
   return (
     <div>
-      <TaskList />
-      <CharacterDisplay />
+      <TaskList characterDisplayRef={characterDisplayRef} />
+      <CharacterDisplay ref={characterDisplayRef} />
     </div>
   );
 };
