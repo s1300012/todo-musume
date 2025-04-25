160048dceaeb4eaf6c21ae7a339b38b94ea22e0b
鈴木友也 <suzy_tomy@docomo.ne.jp>
2025-04-25 09:15:40 +0900

    bらざー

--- src/utils/music/charaSE.ts
+++ src/utils/music/charaSE.ts
@@ -1,5 +1,5 @@
-import chara1_greet from "../../assets/music/chara1/おにーさん2.mp3"
-import chara2_greet from "../../assets/music/chara2/よろしく.mp3"
-import chara3_greet from "../../assets/music/chara3/よんだかな.mp3"
+import chara1_greet from "../../assets/music/chara1/chara1_brother.mp3"
+import chara2_greet from "../../assets/music/chara2/chara2_yorosiku.mp3"
+import chara3_greet from "../../assets/music/chara3/chara3_yondakana.mp3"
 
 export const greets = [ {id: 1, music: chara1_greet}, {id : 2, music: chara2_greet}, {id: 3, music: chara3_greet}];
